6b9a1e91597579ba364eba499e219c24d86030b5
idarerick <darrick.mail@gmail.com>
2018-08-28 23:23:35 -0700

    test

---
+++ config.py
@@ -0,0 +1,8 @@
+# Twitter API Keys
+consumer_key = "ZsNXc0ar95HPNT1sqYlgTtWl9"
+consumer_secret = "P2IvDA95rjMZ0Z3eDvuT1Z6myCxYaErKjiyz4lg6U8Sl5cf9hY"
+access_token = "525037343-K8VGETdKhVBEI4k7En5joU33wm8l2rcxLHFqIkBm"
+access_token_secret = "cldM8IdBphruWra9Ur3v6vETipipsObzpS92rzcO0gS7p"
+
+# OpenWeatherMap API Key
+api_key = "1cf4b7374139e1951e37d7cd31406501"
